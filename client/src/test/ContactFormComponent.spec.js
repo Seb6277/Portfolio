27816637cde58f009236ed0e9144f05349854a14
Sebastien <seb75@fedoraproject.org>
2019-07-01 11:30:17 +0200

    remove captcha test

--- client/src/test/ContactFormComponent.spec.js
+++
@@ -1,8 +0,0 @@
-import ContactFormComponent from '../components/contact/ContactFormComponent'
-
-describe('<ContactFormComponent/>', () => {
-  it('should render correctly', async () => {
-    const wrapper = await shallow(<ContactFormComponent/>)
-    expect(wrapper).toMatchSnapshot()
-  });
-})
