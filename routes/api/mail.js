6c925da4bfe1693c505b5b67f79bdbe9d902a905
Sebastien <seb75@fedoraproject.org>
2019-10-21 22:37:54 +0200

    added mailjet

--- routes/api/mail.js
+++ routes/api/mail.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+const mailjet = require('node-mailjet').connect(
+    process.env.MJ_APIKEY_PUBLIC,
+    process.env.MJ_APIKEY_PRIVATE
+)
+
 router.post("/send_mail", function(req, res) {
 
-    // Send email here
+    const request = mailjet.post('send').request({
+        FromEmail: 'sebastien.carpentier89@gmail.com',
+        FromName: 'Mailjet Pilot',
+        Subject: 'contact',
+        'Text-part': `${req.body.userMail} written :
+        ${req.body.message}`,
+        Recipients: [{Email: 'sebastien.carpentier89@gmail.com'}]
+    })
 
-    res.status(200).json("Email sent").end();
+    request.then(result => {
+        res.status(200).json({success: true}).end()
+    }).catch(error => {
+        res.status(500).json({sucess: false, log: error}).end()
+    })
 });
 
 module.exports = router;
